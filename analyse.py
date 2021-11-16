6eb5580880742c1cfe329412c2125f6a8d182084
tiago-falves <tiago.falves98@gmail.com>
2021-11-16 12:01:39 +0000

    Added tags.csv

--- analyse.py
+++ analyse.py
@@ -1,15 +1,25 @@
 import csv
 import pandas
+import matplotlib
+import matplotlib.pyplot as plt
 
 
-df = pandas.read_csv("data.csv", delimiter=";", encoding="ISO-8859-1")
-
-df['tags'].tolist()
-
-
-df.describe(include="all").to_csv("describe.csv")
+def to_1D(series):
+    return pandas.Series([x for _list in series for x in _list])
 
 
+df = pandas.read_csv("datatemp.csv", delimiter=";", encoding="ISO-8859-1")
+
+# Transformar a string tags numa lista
+df["tags"] = df["tags"].apply(eval)
+to_1D(df["tags"]).value_counts().to_csv("tags.csv")
+
+
+# df.describe(include="all").to_csv("describe.csv")
+# df.plot()
+# df.groupby('tags')['tags'].count().plot.pie(
+#    autopct='%.2f', figsize=(5, 5))
+# plt.show()
 # print(df['date'].value_counts())
 
 # print(type(df["tags"][0]))
