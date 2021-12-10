c8e2849ec713cefe957f24c4da7473778e1a2179
tiago-falves <tiago.falves98@gmail.com>
2021-12-10 15:09:25 +0000

    Solr recognizing dates

--- Milestone_2/csv_to_json.py
+++ Milestone_2/csv_to_json.py
@@ -1,15 +1,31 @@
 import pandas as pd
+from datetime import datetime
 
-csv_file = pd.DataFrame(pd.read_csv("data_clean_1-608.csv", sep=";", header=0, index_col=False, encoding="UTF-8"))
 
+csv_file = pd.DataFrame(pd.read_csv(
+    "data_clean_1-608.csv", sep=";", header=0, index_col=False, encoding="UTF-8"))
+
+
+def parse_datetime(value):
+    months = {"jan": 1, "fev": 2, "mar": 3, "abr": 4, "mai": 5, "jun": 6,
+              "jul": 7, "ago": 8, "set": 9, "out": 10, "nov": 11, "dez": 12}
+    value = value.split()
+    value[1] = months[value[1]]
+    time = str(value[3]).split(':')
+    value = "{Y}-{m}-{d}T{H}:{min}:00Z".format(
+        Y=value[2], m=value[1], d=value[0], H=time[0], min=time[1])
+    return value
 
 
 def to_list(x):
     return x[2:-2].split("', '")
 
+
+csv_file['datetime'] = csv_file['date'] + " " + csv_file['time']
+csv_file['datetime'] = csv_file['datetime'].apply(parse_datetime)
+print(csv_file['datetime'])
+csv_file.to_csv("tags.csv")
 csv_file["partner"] = csv_file["partner"].apply(to_list)
 csv_file["tags"] = csv_file["tags"].apply(to_list)
 
 csv_file.to_json("data.json", orient="records", force_ascii=True)
-
-
