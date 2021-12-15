62114dcd59308b052f64d0cb60c1e7d1381a377c
tiago-falves <tiago.falves98@gmail.com>
2021-12-15 22:16:42 +0000

    Added Querie 3

--- Milestone_2/evaluate.py
+++ Milestone_2/evaluate.py
@@ -102,12 +102,27 @@ def qrelFiles(num_queries):
     return qrels
 
 
+def qrelFilesBoosted(num_queries):
+    qrels = []
+    for index in range(1, num_queries + 1):
+        qrels.append(f'Qrels_boosted/query_{index}.txt')
+    return qrels
+
+
 QUERY_URL = [
     'http://localhost:8983/solr/news/query?q=(%0A%20%20%20%20(title:%22pol%C3%ADtica%22%20OR%20title:%22governo%22%20OR%20title:%22partido%22)%5E2%20OR%20(text:%22pol%C3%ADtica%22%20OR%20text:%22governo%22%20OR%20text:%22partido%22)%0A%20%20%20%20OR%20(tags:%22Aut%C3%A1rquicas2021%22%20OR%20tags:%22PSD%22)%0A)%20AND%20datetime:%5B%20NOW-1MONTHS%20TO%20NOW%5D&q.op=OR&indent=true&wt=json',
-    'http://localhost:8983/solr/news/query?q=(%0A%20%20%20%20(%0A%20%20%20%20%20%20%20%20title:%22Marcelo%20Rebelo%20de%20Sousa%22%0A%20%20%20%20%20%20%20%20OR%20text:%22Marcelo%20Rebelo%20de%20Sousa%22%0A%20%20%20%20%20%20%20%20OR%20tags:%22Marcelo%20Rebelo%20de%20Sousa%22%5E2%0A%20%20%20%20%20%20%20%20OR%20title:%22presidente%22%0A%20%20%20%20%20%20%20%20OR%20text:%22presidente%22%0A%20%20%20%20)%0A%20%20%20%20AND%0A%20%20%20%20(%0A%20%20%20%20%20%20%20%20title:%22Antonio%20Costa%22%0A%20%20%20%20%20%20%20%20OR%20text:%22Antonio%20Costa%22%0A%20%20%20%20%20%20%20%20OR%20tags:%22Antonio%20Costa%22%5E2%0A%20%20%20%20%20%20%20%20OR%20title:%22primeiro-ministro%22%0A%20%20%20%20%20%20%20%20OR%20text:%22primeiro-ministro%22%0A%20%20%20%20)%0A)%20AND%20datetime:%5BNOW-30DAYS%20TO%20NOW%5D&q.op=OR&indent=true&wt=json&qt='
+    'http://localhost:8983/solr/news/query?q=(%0A%20%20%20%20(%0A%20%20%20%20%20%20%20%20title:%22Marcelo%20Rebelo%20de%20Sousa%22%0A%20%20%20%20%20%20%20%20OR%20text:%22Marcelo%20Rebelo%20de%20Sousa%22%0A%20%20%20%20%20%20%20%20OR%20tags:%22Marcelo%20Rebelo%20de%20Sousa%22%5E2%0A%20%20%20%20%20%20%20%20OR%20title:%22presidente%22%0A%20%20%20%20%20%20%20%20OR%20text:%22presidente%22%0A%20%20%20%20)%0A%20%20%20%20AND%0A%20%20%20%20(%0A%20%20%20%20%20%20%20%20title:%22Antonio%20Costa%22%0A%20%20%20%20%20%20%20%20OR%20text:%22Antonio%20Costa%22%0A%20%20%20%20%20%20%20%20OR%20tags:%22Antonio%20Costa%22%5E2%0A%20%20%20%20%20%20%20%20OR%20title:%22primeiro-ministro%22%0A%20%20%20%20%20%20%20%20OR%20text:%22primeiro-ministro%22%0A%20%20%20%20)%0A)%20AND%20datetime:%5BNOW-30DAYS%20TO%20NOW%5D&q.op=OR&indent=true&wt=json&qt=',
+    'http://localhost:8983/solr/news/query?q=Covid-19&q.op=OR&defType=dismax&indent=true&qf=title%20tags%20excerpt%20text&fq=text_length:%5B0%20TO%201000%5D'
+]
+
+QUERY_URL_BOOSTED = [
+    'http://localhost:8983/solr/news/query?q=(%0A%20%20%20%20(title:%22pol%C3%ADtica%22%20OR%20title:%22governo%22%20OR%20title:%22partido%22)%5E2%20OR%20(text:%22pol%C3%ADtica%22%20OR%20text:%22governo%22%20OR%20text:%22partido%22)%0A%20%20%20%20OR%20(tags:%22Aut%C3%A1rquicas2021%22%20OR%20tags:%22PSD%22)%0A)%20AND%20datetime:%5B%20NOW-1MONTHS%20TO%20NOW%5D&q.op=OR&indent=true&wt=json',
+    'http://localhost:8983/solr/news/query?q=(%0A%20%20%20%20(%0A%20%20%20%20%20%20%20%20title:%22Marcelo%20Rebelo%20de%20Sousa%22%0A%20%20%20%20%20%20%20%20OR%20text:%22Marcelo%20Rebelo%20de%20Sousa%22%0A%20%20%20%20%20%20%20%20OR%20tags:%22Marcelo%20Rebelo%20de%20Sousa%22%5E2%0A%20%20%20%20%20%20%20%20OR%20title:%22presidente%22%0A%20%20%20%20%20%20%20%20OR%20text:%22presidente%22%0A%20%20%20%20)%0A%20%20%20%20AND%0A%20%20%20%20(%0A%20%20%20%20%20%20%20%20title:%22Antonio%20Costa%22%0A%20%20%20%20%20%20%20%20OR%20text:%22Antonio%20Costa%22%0A%20%20%20%20%20%20%20%20OR%20tags:%22Antonio%20Costa%22%5E2%0A%20%20%20%20%20%20%20%20OR%20title:%22primeiro-ministro%22%0A%20%20%20%20%20%20%20%20OR%20text:%22primeiro-ministro%22%0A%20%20%20%20)%0A)%20AND%20datetime:%5BNOW-30DAYS%20TO%20NOW%5D&q.op=OR&indent=true&wt=json&qt=',
+    'http://localhost:8983/solr/news/query?q=Covid-19&q.op=OR&defType=dismax&indent=true&qf=title%5E10%20tags%5E5%20excerpt%5E1.2%20text%5E0.8&bf=product(recip(ms(NOW,datetime),1,1,1),recip(text_length,1,1,1))%5E1e15'
 ]
 
 QRELS_FILES = qrelFiles(len(QUERY_URL))
+QRELS_FILES_BOOSTED = qrelFilesBoosted(len(QUERY_URL_BOOSTED))
 
 for idx in range(len(QUERY_URL)):
 
@@ -119,6 +134,22 @@ for idx in range(len(QUERY_URL)):
 
     latex_name = f'Results/results_{idx+1}.tex'
     file_name = f'Results/precision_recall_{idx+1}.pdf'
+
+    with open(latex_name, 'w') as tf:
+        tf.write(metrics_table(results, relevant).to_latex())
+
+    precision_call_curve(results, relevant, file_name)
+
+for idx in range(len(QUERY_URL_BOOSTED)):
+
+    # Read qrels to extract relevant documents
+    relevant = list(map(lambda el: el.strip(),
+                    open(QRELS_FILES_BOOSTED[idx]).readlines()))
+    # Get query results from Solr instance
+    results = requests.get(QUERY_URL[idx]).json()['response']['docs']
+
+    latex_name = f'Results/Boosted/results_{idx+1}.tex'
+    file_name = f'Results/Boosted/precision_recall_{idx+1}.pdf'
 
     with open(latex_name, 'w') as tf:
         tf.write(metrics_table(results, relevant).to_latex())
