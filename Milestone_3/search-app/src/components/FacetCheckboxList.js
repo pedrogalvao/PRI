6d95b69904677d68a8a8984c4f0eaf843e9759f8
tiago-falves <tiago.falves98@gmail.com>
2022-01-20 17:23:12 +0000

    Tags working again

--- Milestone_3/search-app/src/components/FacetCheckboxList.js
+++ Milestone_3/search-app/src/components/FacetCheckboxList.js
@@ -16,7 +16,8 @@ export default function FacetCheckboxList({facets, counts, setNews, params}) {
 
     var startDate = urlParams.get("startDate")
     var endDate = urlParams.get("endDate")
-      if(params["fq"] == null){
+
+      if(params["fq"] == null || params["fq"] == ""){
         if (startDate !== null && endDate == null ) {
             params["fq"] = `datetime:[${startDate} TO NOW]`;
           }else if (startDate !== null && endDate != null) {
@@ -43,21 +44,19 @@ export default function FacetCheckboxList({facets, counts, setNews, params}) {
       timeout: 4000
     });
 
-    const zas = params
 
-
-    solr.get('/select', {params: zas})
+    solr.get('/select', {params: params})
       .then(function (response) {
         if (response.data.response.numFound !== 0){
            setNews(response.data.response.docs)
         }  
         else{
           //TODO Por NOTFOUND
-          console.log('error 1 novo ')
+          console.log('Não foram encontradas notícias')
         }
       })
       .catch((err) => {
-        console.log('error 2 novo')
+        console.log('Error 2')
         console.log(err.message)
       })
 
@@ -65,65 +64,6 @@ export default function FacetCheckboxList({facets, counts, setNews, params}) {
 
 
   
-
-  function removeItemOnce(arr, value) {
-    var index = arr.indexOf(value);
-    if (index > -1) {
-      arr.splice(index, 1);
-    }
-    return arr;
-  }
-
-  function addParams(tag){
-
-      if(params["fq"]==null || params["fq"]==[] || params["fq"]==""){
-        params["fq"] = 'tags:' + tag 
-         
-      }else{
-        params["fq"] = params["fq"] + " && tags:" + tag 
-      }
-      console.log(params["fq"])
-  }
-  function removeParams(tag){
-
-    if(params["fq"]==null || params["fq"]==[] || params["fq"]==""){
-
-    }else{
-        params["fq"] = params["fq"].split(" && ");
-        params["fq"] = removeItemOnce(params["fq"], "tags:"+tag);
-        if(params["fq"].length == 0){
-            params["fq"] =""
-        }else {
-            params["fq"].join("&&");
-        }
-    }
-
-    // if(typeof(params["fq"]) == )
-}
-
-  
-   
-
-  const handleOnChange = (position) => {
- 
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item  
-    );
-    setCheckedState(updatedCheckedState);
-
-   
-
-    if(!checkedState[position]){
-        addParams(facets[position])
-    } 
-    else {
-        console.log(params)
-        removeParams(facets[position])
-    }
-    // else removeParams(facets[position])
-    getNews(params);
-    
-  };
 
   function buildFq(newFormats){
 
